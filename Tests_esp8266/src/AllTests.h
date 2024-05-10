3b9cfdf04bc594e4b964ea4af46fb44d63d82357
Vitalii Orazov <viordash@mail.ru>
2024-05-10 12:29:43 +0300

    add hotreload tests

--- Tests_esp8266/src/AllTests.h
+++ Tests_esp8266/src/AllTests.h
@@ -8,6 +8,9 @@ TEST_C_WRAPPER(Crc32TestsGroup, data_16);
 TEST_C_WRAPPER(Crc32TestsGroup, empty_data);
 TEST_C_WRAPPER(Crc32TestsGroup, null_data);
 
-
-TEST_GROUP_C_WRAPPER(HotReloadTestsGroup){};
+TEST_GROUP_C_WRAPPER(HotReloadTestsGroup) {
+    TEST_GROUP_C_SETUP_WRAPPER(HotReloadTestsGroup);
+    TEST_GROUP_C_TEARDOWN_WRAPPER(HotReloadTestsGroup);
+};
 TEST_C_WRAPPER(HotReloadTestsGroup, store_load);
+TEST_C_WRAPPER(HotReloadTestsGroup, load_if_memory_cleared);
--- Tests_esp8266/src/hotreload_tests.c
+++ Tests_esp8266/src/hotreload_tests.c
@@ -3,11 +3,18 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 static uint8_t testable[256] = {};
-#define RTC_USER_BASE testable
 
+TEST_GROUP_C_SETUP(HotReloadTestsGroup) {
+    memset(testable, 0, sizeof(testable));
+}
+TEST_GROUP_C_TEARDOWN(HotReloadTestsGroup) {
+}
+
+#define RTC_USER_BASE testable
 #include "hotreload_service.c"
 
 TEST_C(HotReloadTestsGroup, store_load) {
@@ -21,4 +28,10 @@ TEST_C(HotReloadTestsGroup, store_load) {
     CHECK_EQUAL_C_BOOL(true, try_load_hotreload(&load_data));
 
     CHECK_EQUAL_C_MEMCMP(&store_data, &load_data, sizeof(store_data));
+}
+
+TEST_C(HotReloadTestsGroup, load_if_memory_cleared) {
+    hotreload load_data = {};
+
+    CHECK_EQUAL_C_BOOL(false, try_load_hotreload(&load_data));
 }
