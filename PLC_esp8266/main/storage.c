084e939aa9d712857c1d1dc7e255e9704a92733c
Vitalii Orazov <viordash@mail.ru>
2024-05-11 12:03:07 +0300

    wip. add storages module (spiffs)

---
+++ PLC_esp8266/main/storage.c
@@ -0,0 +1,30 @@
+
+#include "storage.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_spiffs.h"
+
+static const char *TAG = "storage";
+
+void open_storage(const char *partition, const char *path) {
+    ESP_LOGI(TAG, "Initializing storage '%s'", partition);
+
+    esp_vfs_spiffs_conf_t conf = { .base_path = path,
+                                   .partition_label = partition,
+                                   .max_files = 3,
+                                   .format_if_mount_failed = true };
+
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    if (ret == ESP_FAIL) {
+        ESP_LOGE(TAG, "Failed to mount or format filesystem");
+    } else if (ret == ESP_ERR_NOT_FOUND) {
+        ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+    } else {
+        ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+    }
+}
+
+void close_storage(const char *partition) {
+    esp_vfs_spiffs_unregister(partition);
+    ESP_LOGI(TAG, "unmount storage '%s'", partition);
+}
---
+++ PLC_esp8266/main/storage.h
@@ -0,0 +1,4 @@
+
+
+void open_storage(const char *partition, const char *path);
+void close_storage(const char *partition);
