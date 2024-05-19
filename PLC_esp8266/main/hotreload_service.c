ac6f458c8fa4f5c119c04ed4362ec3525a70dead
viordash <viordash@mail.ru>
2024-05-19 12:52:34 +0300

    to more strict hotreload service

--- PLC_esp8266/main/hotreload_service.c
+++ PLC_esp8266/main/hotreload_service.c
@@ -6,19 +6,32 @@
 
 static const char *TAG = "hotreload";
 
+static const uint32_t MAGIC = 0xDE4572BB;
+
+typedef uint32_t t_crc;
+
 typedef struct {
     hotreload data;
-    uint32_t crc;
+    union {
+        uint32_t magic;
+        uint8_t dummy[256 - sizeof(hotreload) - sizeof(t_crc)];
+    };
+    t_crc crc;
 } hotreload_data;
 
 volatile hotreload_data *_hotreload_data = (volatile hotreload_data *)RTC_USER_BASE;
 
 bool try_load_hotreload(hotreload *data) {
-    if (_hotreload_data->crc
-        != calc_crc32(CRC32_INIT,
-                      (const void *)&_hotreload_data->data,
-                      sizeof(_hotreload_data->data))) {
+    if (_hotreload_data->magic != MAGIC) {
+        ESP_LOGW(TAG, "try_load_hotreload, incorrect magic\r\n");
+        return false;
+    }
 
+    t_crc crc =
+        calc_crc32(CRC32_INIT, (const void *)&_hotreload_data->data, sizeof(_hotreload_data->data));
+    crc = calc_crc32(crc, (const void *)&_hotreload_data->dummy, sizeof(_hotreload_data->dummy));
+
+    if (_hotreload_data->crc != crc) {
         ESP_LOGW(TAG, "try_load_hotreload, wrong crc\r\n");
         return false;
     }
@@ -27,6 +40,9 @@ bool try_load_hotreload(hotreload *data) {
 }
 
 void store_hotreload(hotreload *data) {
-    _hotreload_data->crc = calc_crc32(CRC32_INIT, data, sizeof(*data));
+    _hotreload_data->magic = MAGIC;
+    t_crc crc = calc_crc32(CRC32_INIT, (const void *)data, sizeof(*data));
+    crc = calc_crc32(crc, (const void *)&_hotreload_data->dummy, sizeof(_hotreload_data->dummy));
+    _hotreload_data->crc = crc;
     _hotreload_data->data = *data;
 }
