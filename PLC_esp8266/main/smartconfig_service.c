8ec15655dc80c775e6e810a49a2876fee5ef8851
viordash <viordash@mail.ru>
2024-05-19 23:25:14 +0300

    wip. entering to smartconfig mode

--- PLC_esp8266/main/smartconfig_service.c
+++ PLC_esp8266/main/smartconfig_service.c
@@ -16,9 +16,9 @@
 #include <string.h>
 
 static const char *TAG = "smartconfig";
-static const uint32_t min_period_ms = 1000;
-static const uint32_t max_period_ms = 5000;
-static const uint32_t start_smartconfig_counter = 3;
+static const uint32_t min_period_ms = 500;
+static const uint32_t max_period_ms = 4000;
+static const uint32_t start_smartconfig_counter = 4;
 
 extern device_settings settings;
 
@@ -30,16 +30,13 @@ static void start_smartconfig() {
 static void smartconfig_task(void *parm) {
     ESP_LOGI(TAG, "Start task");
 
-    TickType_t ticks_start = 0;
-    vTaskDelayUntil(&ticks_start, min_period_ms / portTICK_RATE_MS);
-
     settings.smartconfig.counter++;
-
-    ESP_LOGI(TAG, "Begin check period, %u", settings.smartconfig.counter);
-
-    bool ready_to_smartconfig = settings.smartconfig.counter >= start_smartconfig_counter;
+    bool ready_to_smartconfig = settings.smartconfig.counter == start_smartconfig_counter;
 
     if (!ready_to_smartconfig) {
+        TickType_t ticks_start = 0;
+        vTaskDelayUntil(&ticks_start, min_period_ms / portTICK_RATE_MS);
+        ESP_LOGI(TAG, "Begin check period, %u", settings.smartconfig.counter);
         store_settings();
         vTaskDelayUntil(&ticks_start, max_period_ms / portTICK_RATE_MS);
         ESP_LOGI(TAG, "End check period, %u", settings.smartconfig.counter);
