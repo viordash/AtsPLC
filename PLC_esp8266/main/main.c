c10299f0c0ca9684d59fd0f3d29af354237a245b
viordash <viordash@mail.ru>
2024-06-01 23:20:55 +0300

    log free mem

--- PLC_esp8266/main/main.c
+++ PLC_esp8266/main/main.c
@@ -86,16 +86,18 @@ void app_main() {
         start_wifi_sta();
     }
 
+    int wifi_restart = 980;
     for (int i = 1000; i >= 0; i -= 5) {
-        if (i < 980) {
+        if (i < wifi_restart) {
             if (wifi_sta_is_runned()) {
                 stop_wifi_sta();
-            } else if (i < 970 && has_wifi_sta_settings) {
+            } else if (i < wifi_restart - 10 && has_wifi_sta_settings) {
                 start_wifi_sta();
+                wifi_restart -= 100;
             }
         }
 
-        printf("Restarting in %d seconds...\n", i);
+        printf("Restarting in %d seconds...  [free mem:%u]\n", i, esp_get_free_heap_size());
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 
