a32f21bc08e6386df765b76e139bdeb867503469
viordash <viordash@mail.ru>
2024-07-14 23:25:02 +0300

    wip. ladder diagram

--- PLC_esp8266/main/Display/display.cpp
+++ PLC_esp8266/main/Display/display.cpp
@@ -38,6 +38,7 @@ static struct {
 // const font_info_t *font = font_builtin_fonts[FONT_FACE_GLCD5x7];
 
 void ladder_diagram(int8_t x, int8_t y);
+void ladder_diagram_acsii(int8_t x, int8_t y);
 
 void display_init() {
     i2c_config_t conf;
@@ -64,71 +65,13 @@ void display_init() {
                         OLED_COLOR_WHITE,
                         OLED_COLOR_BLACK);
 
-    ssd1306_draw_string(&display.dev,
-                        display.buffer,
-                        display.font_6X12,
-                        0,
-                        8,
-                        "002.IN0 | IN1 ? OUT0<0",
-                        OLED_COLOR_WHITE,
-                        OLED_COLOR_BLACK);
-
-    ladder_diagram(16, 21);
-
-    // ssd1306_draw_string(&display.dev,
-    //                     display.buffer,
-    //                     display.font_8X14,
-    //                     0,
-    //                     21,
-    //                     "003.IN0 | IN1 ? OUT0<0",
-    //                     OLED_COLOR_WHITE,
-    //                     OLED_COLOR_BLACK);
-
-    // ssd1306_draw_string(&display.dev,
-    //                     display.buffer,
-    //                     font_builtin_fonts[FONT_FACE_TERMINUS_6X12_ISO8859_1],
-    //                     0,
-    //                     24,
-    //                     "Hello, esp-rtos 2!",
-    //                     OLED_COLOR_WHITE,
-    //                     OLED_COLOR_BLACK);
-
-    // ssd1306_draw_string(&display.dev,
-    //                     display.buffer,
-    //                     font_builtin_fonts[FONT_FACE_TERMINUS_6X12_ISO8859_1],
-    //                     0,
-    //                     36,
-    //                     "Hello, esp-rtos 3!",
-    //                     OLED_COLOR_WHITE,
-    //                     OLED_COLOR_BLACK);
-
-    // ssd1306_draw_string(&display.dev,
-    //                     display.buffer,
-    //                     font_builtin_fonts[FONT_FACE_TERMINUS_8X14_ISO8859_1],
-    //                     0,
-    //                     48,
-    //                     "Hello, esp-rtos 4!",
-    //                     OLED_COLOR_WHITE,
-    //                     OLED_COLOR_BLACK);
+    ladder_diagram(16, 12);
+    ladder_diagram(16, 29);
+    ladder_diagram(16, 46);
+    // ladder_diagram_acsii(14, 32);
 
     ESP_ERROR_CHECK(ssd1306_load_frame_buffer(&display.dev, display.buffer) != 0 ? ESP_FAIL
                                                                                  : ESP_OK);
-
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    // #define CIRCLE_COUNT_ICON_X 64
-    // #define CIRCLE_COUNT_ICON_Y 48
-    //     for (uint8_t i = 0; i < 10; i++) {
-    //         ssd1306_draw_circle(&display.dev,
-    //                             buffer,
-    //                             CIRCLE_COUNT_ICON_X,
-    //                             CIRCLE_COUNT_ICON_Y,
-    //                             i,
-    //                             OLED_COLOR_WHITE);
-    //         ssd1306_load_frame_buffer(&display.dev, buffer);
-    //         vTaskDelay(100 / portTICK_PERIOD_MS);
-    //     }
-
     ESP_LOGI(TAG, "init succesfully");
 }
 
@@ -183,4 +126,43 @@ void ladder_diagram(int8_t x, int8_t y) {
     ssd1306_draw_circle(&display.dev, display.buffer, x + 57 + 5, y + 11 + 1, 5, OLED_COLOR_WHITE);
     ssd1306_draw_hline(&display.dev, display.buffer, x + 62 + 5, y + 11 + 1, 7, OLED_COLOR_WHITE);
     ssd1306_draw_vline(&display.dev, display.buffer, x + 74, y, 17, OLED_COLOR_WHITE);
+}
+
+void ladder_diagram_acsii(int8_t x, int8_t y) {
+
+    ssd1306_draw_char(&display.dev,
+                      display.buffer,
+                      display.font_6X12,
+                      x + 21,
+                      y + 0,
+                      '0',
+                      OLED_COLOR_WHITE,
+                      OLED_COLOR_BLACK);
+
+    ssd1306_draw_char(&display.dev,
+                      display.buffer,
+                      display.font_6X12,
+                      x + 49,
+                      y + 0,
+                      '1',
+                      OLED_COLOR_WHITE,
+                      OLED_COLOR_BLACK);
+
+    ssd1306_draw_char(&display.dev,
+                      display.buffer,
+                      display.font_6X12,
+                      x + 77,
+                      y + 0,
+                      'A',
+                      OLED_COLOR_WHITE,
+                      OLED_COLOR_BLACK);
+
+    ssd1306_draw_string(&display.dev,
+                        display.buffer,
+                        display.font_8X14,
+                        x,
+                        y + 12,
+                        "\x15\x12\x16\x15\x12\x16/\x15\x12()\x12\x16",
+                        OLED_COLOR_WHITE,
+                        OLED_COLOR_BLACK);
 }
