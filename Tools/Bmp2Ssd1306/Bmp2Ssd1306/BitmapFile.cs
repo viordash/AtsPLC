dc2ca324e08df05f09d131ec298c6f9d241cf00f
viordash <viordash@mail.ru>
2024-07-17 23:55:22 +0300

    wip. parse bmp

---
+++ Tools/Bmp2Ssd1306/Bmp2Ssd1306/BitmapFile.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Bmp2Ssd1306 {
+    public class BitmapFile {
+        [StructLayout(LayoutKind.Sequential, Pack = 2)]
+        public struct BITMAPFILEHEADER {
+            public ushort bfType;
+            public uint bfSize;
+            public ushort bfReserved1;
+            public ushort bfReserved2;
+            public uint bfOffBits;
+
+            public BITMAPFILEHEADER() {
+                bfSize = StructHelper.Size(this);
+            }
+        }
+
+        public static byte[] ExtractDib(byte[] bytesBmp) {
+            if (bytesBmp.Length < StructHelper.Size<BITMAPFILEHEADER>()) {
+                throw new ArgumentException("bmp size is too small");
+            }
+
+            var header = StructHelper.FromBytes<BITMAPFILEHEADER>(bytesBmp);
+            if (bytesBmp.Length < header.bfSize) {
+                throw new ArgumentException("wrong bmp size");
+            }
+            if (header.bfType != 0x4D42) {
+                throw new ArgumentException("incorrect bmp type");
+            }
+
+            var bmp = bytesBmp.Skip((int)StructHelper.Size<BITMAPFILEHEADER>()).ToArray();
+            if (!BITMAPINFO.TryParse(bmp, out BITMAPINFO bitmapInfo)) {
+                if (!BITMAPV5INFO.TryParse(bmp, out BITMAPV5INFO bitmapV5Info)) {
+                    throw new ArgumentException("bmp info parse error");
+                }
+                if (bitmapV5Info.bmiHeader.bV5BitCount != 1) {
+                    throw new ArgumentException("incorrect bmp bit count");
+                }
+            } else {
+                if (bitmapInfo.bmiHeader.biBitCount != 1) {
+                    throw new ArgumentException("incorrect bmp bit count");
+                }
+            }
+
+            return bmp.Skip((int)bitmapInfo.bmiHeader.biSize).ToArray();
+        }
+
+    }
+}
--- Tools/Bmp2Ssd1306/Bmp2Ssd1306/Program.cs
+++ Tools/Bmp2Ssd1306/Bmp2Ssd1306/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
 
-namespace MyApp {
+namespace Bmp2Ssd1306 {
     internal class Program {
         static void Main(string[] args) {
+            if (args.Count() != 2) {
+                Console.WriteLine("Use: Bmp2Ssd1306 input.bmp output.h");
+            }
+
+            var inputFilename = args[0];
+            var outputFilename = args[1];
+
+            var bytes = File.ReadAllBytes(inputFilename);
+            parseBmpFile(bytes);
+
+
             Console.WriteLine("Hello World!");
+        }
+
+        static void parseBmpFile(byte[] bytes) {
+            var dib = BitmapFile.ExtractDib(bytes);
+            // if (!BitmapFile.ExtractDib(bytes, out BITMAPINFO bitmapInfo)) {
+            //     throw new ArgumentException("bmp file parse error");
+            // }
+            // if (bitmapInfo.bmiHeader.biBitCount != 1) {
+            //     throw new ArgumentException("wrong bmp file format");
+            // }
         }
     }
 }
