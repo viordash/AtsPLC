c8b86cb7057f26238fec6f66daeba1bba5f6f71a
viordash <viordash@mail.ru>
2024-07-20 22:36:32 +0300

    wip. add FlipVertical option to  Bmp2Ssd1306

--- Tools/Bmp2Ssd1306/Bmp2Ssd1306/Program.cs
+++ Tools/Bmp2Ssd1306/Bmp2Ssd1306/Program.cs
@@ -5,8 +5,10 @@ using System.Text;
 namespace Bmp2Ssd1306 {
     internal class Program {
         static void Main(string[] args) {
-            if (args.Length != 2) {
-                Console.WriteLine("Use: Bmp2Ssd1306 input.bmp output.h");
+            if (args.Length < 2) {
+                Console.WriteLine("Use: Bmp2Ssd1306 input.bmp output.h [-fv][-nc]");
+                Console.WriteLine("\t-fv flip vertical");
+                Console.WriteLine("\t-ng negative color");
             }
 
             var inputFilename = args[0];
@@ -15,6 +17,10 @@ namespace Bmp2Ssd1306 {
 
             var bytes = File.ReadAllBytes(inputFilename);
             var dib = ParseBmpFile(bytes);
+
+            if (args.Contains("-fv")) {
+                FlipVertical(dib);
+            }
             var ssd1306 = Convert2Ssd1306(dib);
             var code = CreateCCodeArray(ssd1306, outputArrayName);
             File.WriteAllText(outputFilename, code);
@@ -26,12 +32,24 @@ namespace Bmp2Ssd1306 {
             return dib;
         }
 
+        static void FlipVertical(BitmapFile.Dib dib) {
+            for (int row = 0; row < dib.Height / 2; row++) {
+                for (int column = 0; column < dib.Width / 8; column++) {
+                    var id0 = row * (dib.Width / 8) + column;
+                    var id1 = (dib.Height - 1 - row) * (dib.Width / 8) + column;
+                    var t = dib.Data[id0];
+                    dib.Data[id0] = dib.Data[id1];
+                    dib.Data[id1] = t;
+                }
+            }
+        }
+
         static byte[] Convert2Ssd1306(BitmapFile.Dib dib) {
-            var fb = new byte[dib.Width * dib.Height / 8];
+            var fb = new byte[(dib.Width / 8) * dib.Height];
 
             for (int row = 0; row < dib.Height; row++) {
-                for (int column = 0; column < dib.Width / 8; column++) {
-                    var xbm_offset = row * 16 + column;
+                for (int column = 0; column < (dib.Width / 8); column++) {
+                    var xbm_offset = (row * (dib.Width / 8)) + column;
                     for (byte bit = 0; bit < 8; bit++) {
                         if ((dib.Data[xbm_offset] & 1 << (7 - bit)) != 0) {
                             fb[dib.Width * (row / 8) + column * 8 + bit] |= (byte)(1 << row % 8);
