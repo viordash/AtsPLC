0ad4ace6c9c0e5d38372389d1f797ce963c323ce
viordash <viordash@mail.ru>
2024-07-26 23:18:43 +0300

    fix tests compiling

--- PLC_esp8266/main/LogicProgram/MapIO.h
+++ PLC_esp8266/main/LogicProgram/MapIO.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <map>
+// #include <map>
 
 enum MapIO { DI = 0, AI, O1, O2, V1, V2, V3, V4 };
-// extern const char *MapIONames[];
+extern const char *MapIONames[];
 
-extern std::map<MapIO, const char *> MapIONames;
+// extern std::map<MapIO, const char *> MapIONames;
---
+++ Tests_esp8266/src/LogicProgram.cpp
@@ -0,0 +1,12 @@
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "main/Display/DisplayItemBase.cpp"
+#include "main/Display/LabeledLogicItem.cpp"
+#include "main/LogicProgram/LogicItemBase.cpp"
+#include "main/LogicProgram/MapIO.cpp"
