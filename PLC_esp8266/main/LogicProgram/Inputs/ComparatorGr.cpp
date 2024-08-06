00b6da5ac2dc258ac307d2d69a1ca78fb035cc65
viordash <viordash@mail.ru>
2024-08-06 12:06:28 +0300

    add logic ComparatorGr

---
+++ PLC_esp8266/main/LogicProgram/Inputs/ComparatorGr.cpp
@@ -0,0 +1,31 @@
+#include "LogicProgram/Inputs/ComparatorGr.h"
+#include "Display/bitmaps/cmp_greate_active.h"
+#include "Display/bitmaps/cmp_greate_passive.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+ComparatorGr::ComparatorGr(uint16_t reference, const MapIO io_adr, InputBase &prior_item)
+    : ComparatorBase(reference, io_adr, prior_item) {
+}
+
+ComparatorGr::~ComparatorGr() {
+}
+
+bool ComparatorGr::DoAction() {
+    state =
+        state == LogicItemState::lisActive ? LogicItemState::lisPassive : LogicItemState::lisActive;
+    return true;
+}
+
+const Bitmap *ComparatorGr::GetCurrentBitmap() {
+    switch (state) {
+        case LogicItemState::lisActive:
+            return &ComparatorGr::bitmap_active;
+
+        default:
+            return &ComparatorGr::bitmap_passive;
+    }
+}
---
+++ PLC_esp8266/main/LogicProgram/Inputs/ComparatorGr.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "LogicProgram/Inputs/ComparatorBase.h"
+#include <stdint.h>
+#include <unistd.h>
+
+class ComparatorGr : public ComparatorBase {
+  private:
+    const static Bitmap bitmap_active;
+    const static Bitmap bitmap_passive;
+
+    const Bitmap *GetCurrentBitmap() override final;
+
+  public:
+    ComparatorGr(uint16_t reference, const MapIO io_adr, InputBase &prior_item);
+    ~ComparatorGr();
+
+    bool DoAction() override final;
+};
