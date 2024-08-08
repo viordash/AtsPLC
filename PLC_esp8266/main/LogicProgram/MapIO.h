a4b4d75ed281a70c9c84e1023705dbb0652cfabf
viordash <viordash@mail.ru>
2024-08-08 23:05:15 +0300

    remove extra from MapIO

--- PLC_esp8266/main/LogicProgram/MapIO.h
+++ PLC_esp8266/main/LogicProgram/MapIO.h
@@ -1,4 +1,4 @@
 #pragma once
 
-enum MapIO { Undef = 0, Income, Outcome, DI, AI, O1, O2, V1, V2, V3, V4 };
+enum MapIO { Undef = 0, Income, DI, AI, O1, O2, V1, V2, V3, V4 };
 extern const char *MapIONames[];
