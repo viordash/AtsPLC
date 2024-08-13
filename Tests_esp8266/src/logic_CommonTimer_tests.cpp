7eb5b767d73e001716edab681c48b31cbeadab64
viordash <viordash@mail.ru>
2024-08-13 13:02:35 +0300

    move common timers tests

--- Tests_esp8266/src/logic_CommonTimer_tests.cpp
+++ Tests_esp8266/src/logic_CommonTimer_tests.cpp
@@ -1,6 +1,7 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/MockSupport.h"
 
+#include "esp_timer.h"
 #include <errno.h>
 #include <stdarg.h>
 #include <stdint.h>
@@ -37,6 +38,7 @@ namespace {
             : CommonTimer(incoming_item) {
 
             this->delay_time_us = delay_time_us;
+            this->raise_time_us = (uint64_t)esp_timer_get_time() + delay_time_us;
         }
         virtual ~TestableCommonTimer() {
         }
@@ -53,10 +55,17 @@ namespace {
         LogicItemState *PublicMorozov_Get_state() {
             return &state;
         }
+        uint64_t PublicMorozov_GetLeftTime() {
+            return GetLeftTime();
+        }
+        uint8_t PublicMorozov_GetProgress() {
+            return GetProgress();
+        }
     };
 } // namespace
 
 TEST(LogicCommonTimerTestsGroup, Render_on_top_network) {
+    mock().expectOneCall("esp_timer_get_time").ignoreOtherParameters();
 
     Controller controller;
     IncomeRail incomeRail(controller, 0);
@@ -66,6 +75,7 @@ TEST(LogicCommonTimerTestsGroup, Render_on_top_network) {
 }
 
 TEST(LogicCommonTimerTestsGroup, Render_on_bottom_network) {
+    mock().expectOneCall("esp_timer_get_time").ignoreOtherParameters();
 
     Controller controller;
     IncomeRail incomeRail(controller, 1);
@@ -75,6 +85,8 @@ TEST(LogicCommonTimerTestsGroup, Render_on_bottom_network) {
 }
 
 TEST(LogicCommonTimerTestsGroup, IncomingItemStateHasChanged_has_single_responsibility) {
+    mock().expectNCalls(2, "esp_timer_get_time").ignoreOtherParameters();
+
     Controller controller;
     IncomeRail incomeRail0(controller, 0);
     TestableCommonTimer prev_element(0, &incomeRail0);
@@ -92,4 +104,119 @@ TEST(LogicCommonTimerTestsGroup, IncomingItemStateHasChanged_has_single_responsi
 
     CHECK_TRUE(testable.PublicMorozov_IncomingItemStateHasChanged());
     CHECK_FALSE(testable.PublicMorozov_IncomingItemStateHasChanged());
+}
+
+TEST(LogicCommonTimerTestsGroup, GetLeftTime_when_no_overflowed) {
+    volatile uint64_t os_us = 2;
+    mock()
+        .expectNCalls(7, "esp_timer_get_time")
+        .withOutputParameterReturning("os_us", (const void *)&os_us, sizeof(os_us));
+
+    Controller controller;
+    IncomeRail incomeRail0(controller, 0);
+    TestableCommonTimer testable_0(10, &incomeRail0);
+    uint64_t left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(10, left_time);
+
+    os_us = 10;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(2, left_time);
+
+    os_us = 12 - 1;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(1, left_time);
+
+    os_us = 12;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(0, left_time);
+
+    os_us = 12 + 1;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(0, left_time);
+
+    os_us = UINT64_MAX - 1LL;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(0, left_time);
+}
+
+TEST(LogicCommonTimerTestsGroup, GetLeftTime_when_is_overflowed) {
+    volatile uint64_t os_us = UINT64_MAX - 7;
+    mock()
+        .expectNCalls(8, "esp_timer_get_time")
+        .withOutputParameterReturning("os_us", (const void *)&os_us, sizeof(os_us));
+
+    Controller controller;
+    IncomeRail incomeRail0(controller, 0);
+    TestableCommonTimer testable_0(10, &incomeRail0);
+    uint64_t left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(10, left_time);
+
+    os_us = UINT64_MAX - 4;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(7, left_time);
+
+    os_us = UINT64_MAX;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(3, left_time);
+
+    os_us = 0;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(3 - 1, left_time);
+
+    os_us = 3 - 2;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(1, left_time);
+
+    os_us = 3 - 1;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(0, left_time);
+
+    os_us = UINT64_MAX - 8;
+    left_time = testable_0.PublicMorozov_GetLeftTime();
+    CHECK_EQUAL(0, left_time);
+}
+
+TEST(LogicCommonTimerTestsGroup, GetProgress) {
+    volatile uint64_t os_us = 0;
+    mock()
+        .expectNCalls(10, "esp_timer_get_time")
+        .withOutputParameterReturning("os_us", (const void *)&os_us, sizeof(os_us));
+
+    Controller controller;
+    IncomeRail incomeRail0(controller, 0);
+    TestableCommonTimer testable_0(100, &incomeRail0);
+    uint8_t percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(0, percent);
+
+    os_us = 1;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(1, percent);
+
+    os_us = 5;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(5, percent);
+
+    os_us = 10;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(10, percent);
+
+    os_us = 25;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(25, percent);
+
+    os_us = 50;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(50, percent);
+
+    os_us = 99;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(99, percent);
+
+    os_us = 100;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(100, percent);
+
+    os_us = 120;
+    percent = testable_0.PublicMorozov_GetProgress();
+    CHECK_EQUAL(100, percent);
 }
--- Tests_esp8266/src/logic_timerMSecs_tests.cpp
+++ Tests_esp8266/src/logic_timerMSecs_tests.cpp
@@ -30,10 +30,6 @@ namespace {
         uint64_t PublicMorozov_GetDelayTimeUs() {
             return delay_time_us;
         }
-
-        uint64_t PublicMorozov_GetLeftTime() {
-            return GetLeftTime();
-        }
     };
 } // namespace
 
@@ -53,74 +49,4 @@ TEST(LogicTimerMSecsTestsGroup, Reference_in_limit_1_to_99999) {
 
     TestableTimerMSecs testable_100000(100000, &incomeRail0);
     CHECK_EQUAL(99999 * 1000LL, testable_100000.PublicMorozov_GetDelayTimeUs());
-}
-
-TEST(LogicTimerMSecsTestsGroup, GetLeftTime_when_no_overflowed) {
-    volatile uint64_t os_us = 2 * 1000LL;
-    mock()
-        .expectNCalls(7, "esp_timer_get_time")
-        .withOutputParameterReturning("os_us", (const void *)&os_us, sizeof(os_us));
-
-    Controller controller;
-    IncomeRail incomeRail0(controller, 0);
-    TestableTimerMSecs testable_0(10, &incomeRail0);
-    uint64_t left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(10 * 1000LL, left_time);
-
-    os_us = 10 * 1000LL;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(2 * 1000LL, left_time);
-
-    os_us = 12 * 1000LL - 1;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(1, left_time);
-
-    os_us = 12 * 1000LL;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(0, left_time);
-
-    os_us = 12 * 1000LL + 1;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(0, left_time);
-
-    os_us = UINT64_MAX - 1LL;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(0, left_time);
-}
-
-TEST(LogicTimerMSecsTestsGroup, GetLeftTime_when_is_overflowed) {
-    volatile uint64_t os_us = UINT64_MAX - 7 * 1000LL;
-    mock()
-        .expectNCalls(8, "esp_timer_get_time")
-        .withOutputParameterReturning("os_us", (const void *)&os_us, sizeof(os_us));
-
-    Controller controller;
-    IncomeRail incomeRail0(controller, 0);
-    TestableTimerMSecs testable_0(10, &incomeRail0);
-    uint64_t left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(10 * 1000LL, left_time);
-
-    os_us = UINT64_MAX - 4 * 1000LL;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(7 * 1000LL, left_time);
-
-    os_us = UINT64_MAX;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(3 * 1000LL, left_time);
-
-    os_us = 0;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(3 * 1000LL - 1, left_time);
-
-    os_us = 3 * 1000LL - 2;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(1, left_time);
-
-    os_us = 3 * 1000LL - 1;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(0, left_time);
-
-    os_us = UINT64_MAX - 8 * 1000LL;
-    left_time = testable_0.PublicMorozov_GetLeftTime();
-    CHECK_EQUAL(0, left_time);
 }
