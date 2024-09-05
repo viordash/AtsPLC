672a3d65d753bd24ae5b5497a5812e769a80ff4e
viordash <viordash@mail.ru>
2024-09-05 23:48:38 +0300

    wip. load/store Ladder

--- PLC_esp8266/main/LogicProgram/Ladder.cpp
+++ PLC_esp8266/main/LogicProgram/Ladder.cpp
@@ -1,4 +1,5 @@
 #include "LogicProgram/Ladder.h"
+#include "LogicProgram/Serializer/Record.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include <stdio.h>
@@ -18,4 +19,130 @@ Ladder::~Ladder() {
         ESP_LOGD(TAG_Ladder, "delete network: %p", network);
         delete network;
     }
+}
+
+void Ladder::Append(Network *network) {
+    ESP_LOGI(TAG_Ladder, "append network: %p", network);
+    push_back(network);
+}
+
+void Ladder::Load() {
+    clear();
+    redundant_storage storage = redundant_storage_load(storage_0_partition,
+                                                       storage_0_path,
+                                                       storage_1_partition,
+                                                       storage_1_path,
+                                                       ladder_storage_name);
+
+    SafeLoad(&storage);
+    delete[] storage.data;
+}
+
+void Ladder::Store() {
+    redundant_storage storage;
+    storage.size = Serialize(NULL, 0);
+    if (storage.size == 0) {
+        ESP_LOGE(TAG_Ladder, "serialization setup error");
+        return;
+    }
+    storage.data = new uint8_t[storage.size];
+    storage.version = LADDER_VERSION;
+
+    if (Serialize(storage.data, storage.size) != storage.size) {
+        ESP_LOGE(TAG_Ladder, "serialize error");
+        delete[] storage.data;
+        return;
+    }
+
+    redundant_storage_store(storage_0_partition,
+                            storage_0_path,
+                            storage_1_partition,
+                            storage_1_path,
+                            ladder_storage_name,
+                            &storage);
+    delete[] storage.data;
+}
+
+void Ladder::SafeLoad(redundant_storage *storage) {
+    if (storage->version != LADDER_VERSION) {
+        ESP_LOGE(TAG_Ladder, "wrong storage version: 0x%X", storage->version);
+        return;
+    }
+
+    if (storage->size == 0) {
+        ESP_LOGW(TAG_Ladder, "empty storage");
+    }
+
+    if (Deserialize(storage->data, storage->size) == 0) {
+        ESP_LOGE(TAG_Ladder, "deserialize error");
+    }
+}
+
+size_t Ladder::Deserialize(uint8_t *buffer, size_t buffer_size) {
+    size_t readed = 0;
+
+    uint16_t networks_count;
+    if (!Record::Read(&networks_count, sizeof(networks_count), buffer, buffer_size, &readed)) {
+        return 0;
+    }
+    if (networks_count < Ladder::MinNetworksCount) {
+        return 0;
+    }
+    if (networks_count > Ladder::MaxNetworksCount) {
+        return 0;
+    }
+
+    reserve(networks_count);
+    for (size_t i = 0; i < networks_count; i++) {
+        TvElement tvElement;
+        if (!Record::Read(&tvElement, sizeof(tvElement), buffer, buffer_size, &readed)) {
+            return 0;
+        }
+
+        auto network = new Network();
+        size_t network_readed = network->Deserialize(&buffer[readed], buffer_size - readed);
+        if (network_readed == 0) {
+            delete network;
+            return 0;
+        }
+        readed += network_readed;
+        Append(network);
+    }
+    return readed;
+}
+
+size_t Ladder::Serialize(uint8_t *buffer, size_t buffer_size) {
+    size_t writed = 0;
+
+    uint16_t networks_count = size();
+    if (networks_count < Ladder::MinNetworksCount) {
+        return 0;
+    }
+    if (networks_count > Ladder::MaxNetworksCount) {
+        return 0;
+    }
+
+    if (!Record::Write(&networks_count, sizeof(networks_count), buffer, buffer_size, &writed)) {
+        return 0;
+    }
+
+    for (auto it = begin(); it != end(); ++it) {
+        auto *network = *it;
+        uint8_t *p;
+        bool just_obtain_size = buffer == NULL;
+        if (!just_obtain_size) {
+            p = &buffer[writed];
+        } else {
+            p = NULL;
+        }
+
+        size_t network_writed = network->Serialize(p, buffer_size - writed);
+        bool network_serialize_error = network_writed == 0;
+        if (network_serialize_error) {
+            return 0;
+        }
+        writed += network_writed;
+    }
+
+    return writed;
 }
--- PLC_esp8266/main/LogicProgram/Ladder.h
+++ PLC_esp8266/main/LogicProgram/Ladder.h
@@ -5,10 +5,33 @@
 #include <unistd.h>
 #include <vector>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "partitions.h"
+#include "redundant_storage.h"
+#ifdef __cplusplus
+}
+#endif
+
+#define LADDER_VERSION ((uint32_t)0x20240905)
+
 class Ladder : std::vector<Network *> {
   protected:
+    void SafeLoad(redundant_storage *storage);
+
+    size_t Deserialize(uint8_t *buffer, size_t buffer_size);
+    size_t Serialize(uint8_t *buffer, size_t buffer_size);
 
   public:
+    const static size_t MinNetworksCount = 1;
+    const static size_t MaxNetworksCount = 42;
+
     Ladder();
     ~Ladder();
+
+    void Append(Network *network);
+
+    void Load();
+    void Store();
 };
