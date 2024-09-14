472667e4982cba39fa5fa5df0c181bf8a41b8136
viordash <viordash@mail.ru>
2024-09-15 00:15:35 +0300

    wip. scrolling

--- PLC_esp8266/main/Display/ScrollBar.cpp
+++ PLC_esp8266/main/Display/ScrollBar.cpp
@@ -13,16 +13,18 @@ IRAM_ATTR bool
 ScrollBar::Render(uint8_t *fb, size_t count, size_t viewport_count, size_t view_topindex) {
     bool res = true;
 
-    bool nothing_to_scroll = count == viewport_count;
+    bool nothing_to_scroll = count <= viewport_count;
     if (nothing_to_scroll) {
         return res;
     }
-    uint8_t x = SCROLLBAR_LEFT + 1;
+    uint8_t x = SCROLLBAR_LEFT;
     uint8_t height = SCROLLBAR_HEIGHT / (count / viewport_count);
-    uint8_t y = SCROLLBAR_TOP + (count / (view_topindex + 1));
-    if (y + height > DISPLAY_HEIGHT) {
-        y = DISPLAY_HEIGHT - height;
+    if (height < 3) {
+        height = 3;
     }
+    uint8_t moving_area = SCROLLBAR_HEIGHT - height;
+    uint8_t y_offset = (moving_area * (view_topindex + 0)) / (count - 1);
+    uint8_t y = SCROLLBAR_TOP + y_offset;
 
     ESP_LOGI(TAG_ScrollBar,
              "Render: x:%u, y:%u, height:%u, top:%u, count:%u, viewport_count:%u, view_topindex:%u",
@@ -30,9 +32,9 @@ ScrollBar::Render(uint8_t *fb, size_t count, size_t viewport_count, size_t view_
              y,
              height,
              SCROLLBAR_TOP,
-             count,
-             viewport_count,
-             view_topindex);
+             (uint32_t)count,
+             (uint32_t)viewport_count,
+             (uint32_t)view_topindex);
 
     res = draw_vert_line(fb, x, y, height);
     if (res) {
--- Tests_esp8266/src/ScrollBar_tests.cpp
+++ Tests_esp8266/src/ScrollBar_tests.cpp
@@ -14,7 +14,7 @@
 static uint8_t frame_buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8] = {};
 
 TEST_GROUP(ScrollBarTestsGroup){ //
-                                      TEST_SETUP(){ memset(frame_buffer, 0, sizeof(frame_buffer));
+                                 TEST_SETUP(){ memset(frame_buffer, 0, sizeof(frame_buffer));
 }
 
 TEST_TEARDOWN() {
@@ -34,4 +34,47 @@ TEST(ScrollBarTestsGroup, Render) {
         }
     }
     CHECK_TRUE(any_pixel_coloring);
+}
+
+TEST(ScrollBarTestsGroup, Skip_render_if_nothing_to_scroll) {
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 40, 40, 0));
+
+    bool any_pixel_coloring = false;
+    for (size_t i = 0; i < sizeof(frame_buffer); i++) {
+        if (frame_buffer[i] != 0) {
+            any_pixel_coloring = true;
+            break;
+        }
+    }
+    CHECK_FALSE(any_pixel_coloring);
+}
+
+TEST(ScrollBarTestsGroup, No_screen_overflow) {
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 0, 0, 0));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 100, 2, 0));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 100, 2, 1));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 100, 2, 99));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 4, 2, 0));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 4, 2, 1));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 4, 2, 2));
+    CHECK_TRUE(ScrollBar::Render(frame_buffer, 4, 2, 3));
+
+    bool any_overflow_pixel = false;
+    size_t possible_line1_position_in_buffer = SCROLLBAR_LEFT + (DISPLAY_WIDTH * (INCOME_RAIL_TOP / 8));
+    size_t possible_line2_position_in_buffer = possible_line1_position_in_buffer + 1;
+    for (size_t i = 0; i < sizeof(frame_buffer); i++) {
+        if (frame_buffer[i] != 0) {
+            if (i == possible_line1_position_in_buffer) {
+                possible_line1_position_in_buffer += DISPLAY_WIDTH;
+                continue;
+            }
+            if (i == possible_line2_position_in_buffer) {
+                possible_line2_position_in_buffer += DISPLAY_WIDTH;
+                continue;
+            }
+            any_overflow_pixel = true;
+            break;
+        }
+    }
+    CHECK_FALSE(any_overflow_pixel);
 }
