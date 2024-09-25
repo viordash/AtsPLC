9014da312fc395c8f6a2b0fee7f00a0e15a4eafe
viordash <viordash@mail.ru>
2024-09-26 01:04:50 +0300

    add tests for ElementsBox

--- Tests_esp8266/src/logic_ElementsBox_tests.cpp
+++ Tests_esp8266/src/logic_ElementsBox_tests.cpp
@@ -13,9 +13,11 @@
 #include "main/LogicProgram/Outputs/IncOutput.h"
 
 TEST_GROUP(LogicElementsBoxTestsGroup){ //
-                                        TEST_SETUP(){ mock().disable(); }
+                                        TEST_SETUP(){ mock().disable();
+}
 
-                                        TEST_TEARDOWN(){ mock().enable();
+TEST_TEARDOWN() {
+    mock().enable();
 }
 }
 ;
@@ -30,4 +32,34 @@ TEST(LogicElementsBoxTestsGroup, box_for_outputs_elements) {
     IncOutput stored_element(MapIO::O1);
     ElementsBox testable(100, &stored_element);
     CHECK_EQUAL(4, testable.size());
+}
+
+TEST(LogicElementsBoxTestsGroup, takes_params_from_stored_input_element) {
+    InputNC stored_element(MapIO::V1);
+    ElementsBox testable(100, &stored_element);
+    CHECK_EQUAL(8, testable.size());
+    for (auto *element : testable) {
+        auto *element_as_commonInput = CommonInput::TryToCast(element);
+        if (element_as_commonInput != NULL) {
+            CHECK_EQUAL(MapIO::V1, element_as_commonInput->GetIoAdr());
+        }
+    }
+}
+
+TEST(LogicElementsBoxTestsGroup, takes_params_from_stored_output_element) {
+    IncOutput stored_element(MapIO::O1);
+    ElementsBox testable(100, &stored_element);
+    CHECK_EQUAL(4, testable.size());
+    for (auto *element : testable) {
+        auto *element_as_commonOutput = CommonOutput::TryToCast(element);
+        if (element_as_commonOutput != NULL) {
+            CHECK_EQUAL(MapIO::O1, element_as_commonOutput->GetIoAdr());
+        }
+    }
+}
+
+TEST(LogicElementsBoxTestsGroup, no_available_place_for_timers_and_comparators) {
+    InputNC stored_element(MapIO::V1);
+    ElementsBox testable(30, &stored_element);
+    CHECK_EQUAL(1, testable.size());
 }
