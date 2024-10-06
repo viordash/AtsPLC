c9de154059e7619bf9453793bb84867d6c231f8d
viordash <viordash@mail.ru>
2024-10-06 12:26:34 +0300

    add CommonTimer::TryToCast

--- PLC_esp8266/main/LogicProgram/Inputs/CommonTimer.h
+++ PLC_esp8266/main/LogicProgram/Inputs/CommonTimer.h
@@ -26,4 +26,6 @@ class CommonTimer : public LogicElement {
 
     bool DoAction(bool prev_elem_changed, LogicItemState prev_elem_state) override;
     bool Render(uint8_t *fb, LogicItemState prev_elem_state, Point *start_point) override;
+
+    static CommonTimer *TryToCast(LogicElement *logic_element);
 };
