02fbd1eb676ef6cf67507ece0b01e69868c69f44
viordash <viordash@mail.ru>
2025-03-18 00:13:16 +0300

    testing check_format

--- PLC_esp8266/main/LogicProgram/Controller.cpp
+++ PLC_esp8266/main/LogicProgram/Controller.cpp
@@ -36,6 +36,8 @@ bool Controller::force_process_loop = false;
 EventGroupHandle_t Controller::gpio_events = NULL;
 TaskHandle_t Controller::process_task_handle = NULL;
 
+
+
 Ladder *Controller::ladder = NULL;
 ProcessWakeupService *Controller::processWakeupService = NULL;
 WiFiService *Controller::wifi_service = NULL;
