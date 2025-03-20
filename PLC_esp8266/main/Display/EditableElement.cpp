08e9227c8844120fea7b17064b0c7b3a6a107f4c
Vitalii Orazov <36102143+viordash@users.noreply.github.com>
2025-03-20 16:24:58 +0300

    code format

--- PLC_esp8266/main/Display/EditableElement.cpp
+++ PLC_esp8266/main/Display/EditableElement.cpp
@@ -77,7 +77,6 @@ bool EditableElement::Moving() {
     return editable_state == EditableElement::ElementState::des_Moving;
 }
 
-
 bool EditableElement::Blinking_50() {
     const int blink_timer_us = 0x80000;
     return (esp_timer_get_time() & blink_timer_us) == blink_timer_us;
