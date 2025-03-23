1b6026c7742fde34785688d2ec5cf3ecbd39abcd
viordash <viordash@mail.ru>
2025-03-23 22:18:45 +0300

    Settings. add method for settings validation

--- PLC_esp8266/main/settings.cpp
+++ PLC_esp8266/main/settings.cpp
@@ -95,6 +95,122 @@ void delete_settings() {
     ESP_LOGI(TAG_settings, "deleted");
 }
 
+static bool invalid_symbol(char ch) {
+    return ch < '!' || ch > '~';
+}
+
+static bool invalid_int(int32_t value, int32_t min, int32_t max) {
+    return value < min || value > max;
+}
+
+static bool invalid_uint(uint32_t value, uint32_t min, uint32_t max) {
+    return value < min || value > max;
+}
+
+bool validate_settings(CurrentSettings::device_settings *settings) {
+    size_t pos;
+    char ch;
+
+    pos = 0;
+    while (pos < sizeof(settings->wifi_station.ssid)
+           && (ch = settings->wifi_station.ssid[pos]) != 0) {
+        if (invalid_symbol(ch)) {
+            ESP_LOGI(TAG_settings, "Invalid wifi_station.ssid, '%s'", settings->wifi_station.ssid);
+            return false;
+        }
+        pos++;
+    }
+
+    pos = 0;
+    while (pos < sizeof(settings->wifi_station.password)
+           && (ch = settings->wifi_station.password[pos]) != 0) {
+        if (invalid_symbol(ch)) {
+            ESP_LOGI(TAG_settings,
+                     "Invalid wifi_station.password, '%s'",
+                     settings->wifi_station.password);
+            return false;
+        }
+        pos++;
+    }
+    if (invalid_int(settings->wifi_station.connect_max_retry_count, -1, 7777)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_station.connect_max_retry_count, '%d'",
+                 settings->wifi_station.connect_max_retry_count);
+        return false;
+    }
+    if (invalid_uint(settings->wifi_station.reconnect_delay_ms, 100, 10 * 60 * 1000)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_station.reconnect_delay_ms, '%u'",
+                 settings->wifi_station.reconnect_delay_ms);
+        return false;
+    }
+    if (invalid_uint(settings->wifi_station.scan_station_rssi_period_ms, 100, 10 * 60 * 1000)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_station.scan_station_rssi_period_ms, '%u'",
+                 settings->wifi_station.scan_station_rssi_period_ms);
+        return false;
+    }
+    if (invalid_int(settings->wifi_station.max_rssi, -120, 100)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_station.max_rssi, '%d'",
+                 settings->wifi_station.max_rssi);
+        return false;
+    }
+    if (invalid_int(settings->wifi_station.min_rssi, -120, 100)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_station.min_rssi, '%d'",
+                 settings->wifi_station.min_rssi);
+        return false;
+    }
+    if (settings->wifi_station.min_rssi > settings->wifi_station.max_rssi) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_station.min_rssi > settings->wifi_station.max_rssi, '%d'>'%d'",
+                 settings->wifi_station.min_rssi,
+                 settings->wifi_station.max_rssi);
+        return false;
+    }
+
+    if (invalid_uint(settings->wifi_scanner.per_channel_scan_time_ms, 100, 20 * 1000)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_scanner.per_channel_scan_time_ms, '%u'",
+                 settings->wifi_scanner.per_channel_scan_time_ms);
+        return false;
+    }
+    if (invalid_int(settings->wifi_scanner.max_rssi, -120, 100)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_scanner.max_rssi, '%d'",
+                 settings->wifi_scanner.max_rssi);
+        return false;
+    }
+    if (invalid_int(settings->wifi_scanner.min_rssi, -120, 100)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_scanner.min_rssi, '%d'",
+                 settings->wifi_scanner.min_rssi);
+        return false;
+    }
+    if (settings->wifi_scanner.min_rssi > settings->wifi_scanner.max_rssi) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_scanner.min_rssi > settings->wifi_scanner.max_rssi, '%d'>'%d'",
+                 settings->wifi_scanner.min_rssi,
+                 settings->wifi_scanner.max_rssi);
+        return false;
+    }
+
+    if (invalid_uint(settings->wifi_access_point.generation_time_ms, 100, 10 * 60 * 1000)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_access_point.generation_time_ms, '%u'",
+                 settings->wifi_access_point.generation_time_ms);
+        return false;
+    }
+    if (invalid_uint(settings->wifi_access_point.ssid_hidden, 0, 1)) {
+        ESP_LOGI(TAG_settings,
+                 "Invalid wifi_access_point.ssid_hidden, '%u'",
+                 settings->wifi_access_point.ssid_hidden);
+        return false;
+    }
+    return true;
+}
+
 void lock_settings() {
     ESP_ERROR_CHECK(xSemaphoreTake(mutex, portMAX_DELAY) != pdTRUE ? ESP_ERR_NO_MEM : ESP_OK);
 }
--- PLC_esp8266/main/settings.h
+++ PLC_esp8266/main/settings.h
@@ -12,6 +12,7 @@ extern CurrentSettings::device_settings settings;
 void load_settings();
 void store_settings();
 void delete_settings();
+bool validate_settings(CurrentSettings::device_settings *settings);
 
 void lock_settings();
 void unlock_settings();
