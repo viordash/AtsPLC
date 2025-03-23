8e7caefc8eef8c6a405084d519f56af7622813a0
viordash <viordash@mail.ru>
2025-03-23 22:36:37 +0300

    wip. SettingsElement. add tests for Serialize\Deserialize

--- Tests_esp8266/src/logic_SettingsElement_tests.cpp
+++ Tests_esp8266/src/logic_SettingsElement_tests.cpp
@@ -41,8 +41,9 @@ namespace {
         bool PublicMorozov_ValidateDiscriminator(Discriminator *discriminator) {
             return ValidateDiscriminator(discriminator);
         }
-        bool PublicMorozov_ValidateValue(Discriminator *discriminator, Value *value) {
-            return ValidateValue(discriminator, value);
+
+        Discriminator *PublicMorozov_Get_discriminator() {
+            return &discriminator;
         }
     };
 } // namespace
@@ -102,153 +103,78 @@ TEST(LogicSettingsElementTestsGroup, ValidateDiscriminator) {
     CHECK_FALSE(testable.PublicMorozov_ValidateDiscriminator(&discriminator));
 }
 
-TEST(LogicSettingsElementTestsGroup, ValidateValue_string) {
+TEST(LogicSettingsElementTestsGroup, Serialize) {
+    uint8_t buffer[256] = {};
     TestableSettingsElement testable;
-    SettingsElement::Discriminator discriminator =
+    *testable.PublicMorozov_Get_discriminator() =
+        SettingsElement::Discriminator::t_wifi_scanner_settings_min_rssi;
+
+    size_t writed = testable.Serialize(buffer, sizeof(buffer));
+    CHECK_EQUAL(5, writed);
+
+    CHECK_EQUAL(TvElementType::et_Settings, *((TvElementType *)&buffer[0]));
+    CHECK_EQUAL(SettingsElement::Discriminator::t_wifi_scanner_settings_min_rssi,
+                *((SettingsElement::Discriminator *)&buffer[1]));
+}
+
+TEST(LogicSettingsElementTestsGroup, Serialize_just_for_obtain_size) {
+    TestableSettingsElement testable;
+
+    size_t writed = testable.Serialize(NULL, SIZE_MAX);
+    CHECK_EQUAL(5, writed);
+
+    writed = testable.Serialize(NULL, 0);
+    CHECK_EQUAL(5, writed);
+}
+
+TEST(LogicSettingsElementTestsGroup, Serialize_to_small_buffer_return_zero) {
+    uint8_t buffer[1] = {};
+    TestableSettingsElement testable;
+
+    size_t writed = testable.Serialize(buffer, sizeof(buffer));
+    CHECK_EQUAL(0, writed);
+}
+
+TEST(LogicSettingsElementTestsGroup, Deserialize) {
+    uint8_t buffer[256] = {};
+    *((TvElementType *)&buffer[0]) = TvElementType::et_Settings;
+    *((SettingsElement::Discriminator *)&buffer[1]) =
+        SettingsElement::Discriminator::t_wifi_scanner_settings_per_channel_scan_time_ms;
+
+    TestableSettingsElement testable;
+
+    size_t readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
+    CHECK_EQUAL(4, readed);
+
+    CHECK_EQUAL(SettingsElement::Discriminator::t_wifi_scanner_settings_per_channel_scan_time_ms,
+                *testable.PublicMorozov_Get_discriminator());
+}
+
+TEST(LogicSettingsElementTestsGroup, Deserialize_with_small_buffer_return_zero) {
+    uint8_t buffer[0] = {};
+    *((TvElementType *)&buffer[0]) = TvElementType::et_Settings;
+
+    TestableSettingsElement testable;
+
+    size_t readed = testable.Deserialize(buffer, sizeof(buffer));
+    CHECK_EQUAL(0, readed);
+}
+
+TEST(LogicSettingsElementTestsGroup, Deserialize_with_wrong_discriminator) {
+    uint8_t buffer[256] = {};
+    *((TvElementType *)&buffer[0]) = TvElementType::et_Settings;
+    *((SettingsElement::Discriminator *)&buffer[1]) =
         SettingsElement::Discriminator::t_wifi_station_settings_ssid;
 
-    SettingsElement::Value value;
-    strcpy(value.string_value, "t");
-    CHECK_TRUE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-    strcpy(value.string_value, "string_64_0123456789abcdef0123456789abcdef0123456789abcdef012345");
-    CHECK_TRUE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-
-    strcpy(value.string_value, "string_65_0123456789abcdef0123456789abcdef0123456789abcdef0123456");
-    CHECK_FALSE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-    strcpy(value.string_value,
-           "string_66_0123456789abcdef0123456789abcdef0123456789abcdef01234567");
-    CHECK_FALSE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-    strcpy(value.string_value, "");
-    CHECK_FALSE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-}
-
-TEST(LogicSettingsElementTestsGroup, ValidateValue_bool) {
     TestableSettingsElement testable;
-    SettingsElement::Discriminator discriminator =
-        SettingsElement::Discriminator::t_wifi_access_point_settings_ssid_hidden;
 
-    SettingsElement::Value value;
-    value.bool_value = true;
-    CHECK_TRUE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-    value.bool_value = false;
-    CHECK_TRUE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
+    *((SettingsElement::Discriminator *)&buffer[1]) = (SettingsElement::Discriminator)(
+        SettingsElement::Discriminator::t_wifi_station_settings_ssid - 1);
+    size_t readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
+    CHECK_EQUAL(0, readed);
 
-    value.int32_value = 2;
-    CHECK_FALSE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
-    value.int32_value = -1;
-    CHECK_FALSE(testable.PublicMorozov_ValidateValue(&discriminator, &value));
+    *((SettingsElement::Discriminator *)&buffer[1]) = (SettingsElement::Discriminator)(
+        SettingsElement::Discriminator::t_wifi_access_point_settings_ssid_hidden + 1);
+    readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
+    CHECK_EQUAL(0, readed);
 }
-
-// TEST(LogicSettingsElementTestsGroup, Serialize) {
-//     uint8_t buffer[256] = {};
-//     TestableSettingsElement testable;
-
-//     size_t writed = testable.Serialize(buffer, sizeof(buffer));
-//     CHECK_EQUAL(57, writed);
-
-//     CHECK_EQUAL(TvElementType::et_SettingsElement, *((TvElementType *)&buffer[0]));
-//     CHECK_EQUAL(MapIO::V2, *((MapIO *)&buffer[1]));
-//     STRCMP_EQUAL("ssid", (char *)&buffer[2]);
-//     STRCMP_EQUAL("secret", (char *)&buffer[27]);
-//     STRCMP_EQUAL("0123456789AB", (char *)&buffer[44]);
-// }
-
-// TEST(LogicSettingsElementTestsGroup, Serialize_just_for_obtain_size) {
-//     TestableSettingsElement testable;
-//     testable.SetIoAdr(MapIO::DI);
-//     testable.SetSsid("ssid");
-//     testable.SetPassword("secret");
-//     testable.SetMac("0123456789AB");
-
-//     size_t writed = testable.Serialize(NULL, SIZE_MAX);
-//     CHECK_EQUAL(57, writed);
-
-//     writed = testable.Serialize(NULL, 0);
-//     CHECK_EQUAL(57, writed);
-// }
-
-// TEST(LogicSettingsElementTestsGroup, Serialize_to_small_buffer_return_zero) {
-//     uint8_t buffer[1] = {};
-//     TestableSettingsElement testable;
-//     testable.SetIoAdr(MapIO::DI);
-//     testable.SetSsid("ssid");
-//     testable.SetPassword("secret");
-//     testable.SetMac("0123456789AB");
-
-//     size_t writed = testable.Serialize(buffer, sizeof(buffer));
-//     CHECK_EQUAL(0, writed);
-// }
-
-// TEST(LogicSettingsElementTestsGroup, Deserialize) {
-//     uint8_t buffer[256] = {};
-//     *((TvElementType *)&buffer[0]) = TvElementType::et_SettingsElement;
-//     *((MapIO *)&buffer[1]) = MapIO::V3;
-//     strcpy((char *)&buffer[2], "test_ssid");
-//     strcpy((char *)&buffer[27], "test_secret");
-//     strcpy((char *)&buffer[44], "0123456789AB");
-
-//     TestableSettingsElement testable;
-
-//     size_t readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(56, readed);
-
-//     CHECK_EQUAL(MapIO::V3, testable.GetIoAdr());
-//     CHECK(&Controller::V3 == testable.Input);
-//     STRCMP_EQUAL("test_ssid", testable.GetSsid());
-//     STRCMP_EQUAL("test_secret", testable.GetPassword());
-//     STRCMP_EQUAL("0123456789AB", testable.GetMac());
-// }
-
-// TEST(LogicSettingsElementTestsGroup, Deserialize_with_small_buffer_return_zero) {
-//     uint8_t buffer[0] = {};
-//     *((TvElementType *)&buffer[0]) = TvElementType::et_SettingsElement;
-
-//     TestableSettingsElement testable;
-
-//     size_t readed = testable.Deserialize(buffer, sizeof(buffer));
-//     CHECK_EQUAL(0, readed);
-// }
-
-// TEST(LogicSettingsElementTestsGroup, Deserialize_with_wrong_io_adr_return_zero) {
-//     uint8_t buffer[256] = {};
-//     *((TvElementType *)&buffer[0]) = TvElementType::et_SettingsElement;
-//     strcpy((char *)&buffer[2], "test_ssid");
-//     strcpy((char *)&buffer[27], "test_secret");
-//     strcpy((char *)&buffer[44], "0123456789AB");
-
-//     TestableSettingsElement testable;
-
-//     *((MapIO *)&buffer[1]) = (MapIO)(MapIO::DI - 1);
-//     size_t readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(0, readed);
-
-//     *((MapIO *)&buffer[1]) = (MapIO)(MapIO::V4 + 1);
-//     readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(0, readed);
-
-//     *((MapIO *)&buffer[1]) = MapIO::DI;
-//     readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(56, readed);
-// }
-
-// TEST(LogicSettingsElementTestsGroup, Deserialize_with_wrong_ssid_return_zero) {
-//     uint8_t buffer[256] = {};
-//     *((TvElementType *)&buffer[0]) = TvElementType::et_SettingsElement;
-//     *((MapIO *)&buffer[1]) = MapIO::V3;
-//     buffer[2] = 0;
-//     strcpy((char *)&buffer[27], "test_secret");
-//     strcpy((char *)&buffer[44], "0123456789AB");
-
-//     TestableSettingsElement testable;
-
-//     size_t readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(0, readed);
-
-//     strcpy((char *)&buffer[2], "");
-//     readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(0, readed);
-
-//     strcpy((char *)&buffer[2], "ssid");
-//     readed = testable.Deserialize(&buffer[1], sizeof(buffer) - 1);
-//     CHECK_EQUAL(56, readed);
-// }
