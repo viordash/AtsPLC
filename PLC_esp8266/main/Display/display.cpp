f16f653ad4f460fb6fbf2d273f4f2e4e0948b914
viordash <viordash@mail.ru>
2026-08-25 16:31:52 +0300

    feat: draw outcome rail as a dashed line

--- PLC_esp8266/main/Display/display.cpp
+++ PLC_esp8266/main/Display/display.cpp
@@ -236,27 +236,30 @@ IRAM_ATTR bool draw_outcome_rail(FrameBuffer *fb, uint8_t x, uint8_t y) {
 IRAM_ATTR bool draw_passive_outcome_rail(FrameBuffer *fb, uint8_t x, uint8_t y) {
     int err;
     y -= OUTCOME_RAIL_NETWORK_TOP;
-    uint8_t last_y = y + OUTCOME_RAIL_HEIGHT;
-    err = ssd1306_draw_vline(&display.dev, fb->buffer, x, y, OUTCOME_RAIL_HEIGHT, OLED_COLOR_WHITE);
-    if (err == 0) {
-        int dashed_line_height = OUTCOME_RAIL_HEIGHT / 3;
 
-        uint8_t height = OUTCOME_RAIL_HEIGHT / 3;
-        err = ssd1306_draw_vline(&display.dev, fb->buffer, x + 1, y, height, OLED_COLOR_WHITE);
-        y += height;
+    const uint8_t dash_height = 2;
 
-        ssd1306_color_t color_line = OLED_COLOR_WHITE;
-        while (err == 0 && dashed_line_height >= 2) {
-            err = ssd1306_draw_vline(&display.dev, fb->buffer, x + 1, y, 2, color_line);
-            dashed_line_height -= 2;
-            y += 2;
-            color_line = color_line == OLED_COLOR_BLACK ? OLED_COLOR_WHITE : OLED_COLOR_BLACK;
+    for (size_t i = 0; i < OUTCOME_RAIL_HEIGHT / dash_height; i += 2) {
+
+        err = ssd1306_draw_vline(&display.dev, fb->buffer, x, y, dash_height, OLED_COLOR_WHITE);
+        if (err != 0) {
+            return false;
+        }
+        err = ssd1306_draw_vline(&display.dev, fb->buffer, x + 1, y, dash_height, OLED_COLOR_BLACK);
+        if (err != 0) {
+            return false;
         }
 
-        if (err == 0) {
-            height = last_y - y;
-            ssd1306_draw_vline(&display.dev, fb->buffer, x + 1, y, height, OLED_COLOR_WHITE);
+        y += dash_height;
+        err = ssd1306_draw_vline(&display.dev, fb->buffer, x, y, dash_height, OLED_COLOR_BLACK);
+        if (err != 0) {
+            return false;
         }
+        err = ssd1306_draw_vline(&display.dev, fb->buffer, x + 1, y, dash_height, OLED_COLOR_WHITE);
+        if (err != 0) {
+            return false;
+        }
+        y += dash_height;
     }
     return err == 0;
 }
